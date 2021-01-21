9cf1aa30db207e2bcb0abf7967fc49f765a5ba2b
Tyler Norbury <tylernorbury96@gmail.com>
2021-01-20 17:21:32 -0800

    Create example

---
+++ example/README.md
@@ -0,0 +1,5 @@
+Example for SuggestionTextField
+
+To run the example, cd to this directory and run:
+1. `flutter create .`
+2. `flutter run`
---
+++ example/lib/main.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:suggestion_text_field/suggestion_text_field.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Suggestion Text Field Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<String> submittedTexts = [];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SuggestionTextField(
+              getSuggestions: (String text) {
+                return text.split(" ");
+              },
+              textSubmitted: (String submittedText) {
+                setState(() {
+                  submittedTexts.add(submittedText);
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
